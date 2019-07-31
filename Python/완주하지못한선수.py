7bcf0b68fd131ed8cd5c70e3cc89790807cfe8db
leegangho <fkaus759@naver.com>
2019-07-31 21:50:35 +0900

    프로그래머스 Python 70/100

---
+++ Python/완주하지못한선수.py
@@ -0,0 +1,7 @@
+def solution(participant,completion):
+    p_set=set(participant)
+    c_set=set(completion)
+    answer=list(p_set-c_set)
+    for i in answer:
+        answer2=i
+    return answer2
